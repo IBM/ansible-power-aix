9e5f730113329fca6176cd8b63e47abfa1ed3b6b
dberg1 <dberg1@github.com>
2020-04-10 16:00:00 +0200

    Set "supported_by" to "community" until we get proper certified status.

--- library/nim_suma.py
+++ library/nim_suma.py
@@ -6,7 +6,7 @@
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
-                    'supported_by': 'IBM, Inc'}
+                    'supported_by': 'community'}
 
 DOCUMENTATION = r'''
 ---
