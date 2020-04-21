c2ab382c4cac4922b55e85726ce4db60c2a65175
dberg1 <dberg1@github.com>
2020-04-21 13:59:06 +0200

    Initial inline documentation for module nim_viosupgrade.

--- library/nim_viosupgrade.py
+++ library/nim_viosupgrade.py
@@ -4,18 +4,140 @@
 # Copyright: (c) 2018- IBM, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-"""AIX NIM viosupgrade: tool to upgrade VIOSes in NIM environment"""
-
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = """
+DOCUMENTATION = r'''
 ---
-module: nim_upgradeios
-author: AIX Development Team
-short_description: Perform a upgrade with the viosupgrade tool
-"""
+author:
+- AIX Development Team (@pbfinley1911)
+module: nim_viosupgrade
+short_description: Perform an upgrade with the viosupgrade tool
+description:
+- Tool to upgrade VIOSes in NIM environment.
+version_added: '2.9'
+requirements:
+- AIX >= 7.1 TL3
+- Python >= 2.7
+options:
+  action:
+    description:
+    - Specifies the operation to perform.
+    - C(altdisk_install) to perform and alternate disk install.
+    - C(alt_disk_clean) to cleanup an existing alternate disk install.
+    - C(get_status) to get the status of the upgrade.
+    type: str
+    choices: [ altdisk_install, bos_install, get_status ]
+    required: true
+  vars:
+    description:
+    - Specifies additional parameters.
+    type: dict
+    suboptions:
+      log_file:
+        description:
+        - Specifies path to log file.
+        type: str
+        default: /tmp/ansible_upgradeios_debug.log
+  vios_status:
+    description:
+    - Specifies the result of a previous operation.
+    type: dict
+    suboptions:
+  targets:
+    description:
+    - NIM targets.
+    type: list
+    elements: str
+  target_file_name:
+    description:
+    - File name containing NIM targets in CSV format.
+    type: str
+  mksysb_name:
+    description:
+    - mksysb name.
+    type: dict
+  spot_name:
+    description:
+    - SPOT name.
+    type: dict
+  backup_file:
+    description:
+    - Backup file name.
+    type: dict
+  rootvg_clone_disk:
+    description:
+    - Clone disk name.
+    type: dict
+  rootvg_install_disk:
+    description:
+    - Install disk name.
+    type: dict
+  res_resolv_conf:
+    description:
+    - NIM resolv_conf resource name.
+    type: dict
+  res_script:
+    description:
+    - NIM script resource name.
+    type: dict
+  res_fb_script:
+    description:
+    - NIM fb_script resource name.
+    type: dict
+  res_file_res:
+    description:
+    - NIM file_res resource name.
+    type: dict
+  res_image_data:
+    description:
+    - NIM image_data resource name.
+    type: dict
+  res_log:
+    description:
+    - NIM log resource name.
+    type: dict
+  cluster_exists:
+    description:
+    - Check if cluster exists.
+    type: dict
+  validate_input_data:
+    description:
+    - Validate input data.
+    type: dict
+  skip_rootvg_cloning:
+    description:
+    - Skip rootvg cloning.
+    type: dict
+'''
+
+EXAMPLES = r'''
+- name: Perform an upgrade of nimvios01
+  nim_viosupgrade:
+    targets: nimvios01
+    action: altdisk_install
+'''
+
+RETURN = r'''
+msg:
+    description: Status information.
+    returned: always
+    type: str
+targets:
+    description: List of VIOSes.
+    returned: always
+    type: list
+    elements: str
+nim_node:
+    description: NIM node info.
+    returned: always
+    type: dict
+status:
+    description: Status for each VIOS (dicionnary key).
+    returned: always
+    type: dict
+'''
 
 import logging
 import csv
@@ -246,7 +368,7 @@ def main():
     MODULE = AnsibleModule(
         # TODO: remove not needed attributes
         argument_spec=dict(
-            description=dict(required=False, type='str'),
+            # description=dict(required=False, type='str'),
 
             # IBM automation generic attributes
             action=dict(required=True, type='str',
@@ -263,7 +385,7 @@ def main():
             #                   ] [ -B ] [ -F ] [ -S ] [ -v ]
 
             # mutually exclisive
-            targets=dict(required=False, type='list'),
+            targets=dict(required=False, type='list', elements='str'),
             target_file_name=dict(required=False, type='str'),
 
             # following attributes are dictionaries with
