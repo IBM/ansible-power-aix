c114730f6f6e8cc4dafb5ff664a0291510187b93
Vianney Robin <vianney.robin@atos.net>
2020-04-24 19:18:36 +0200

    travis: fix directories + ansible-test validate-modules

--- devops/bin/gen_doc.sh
+++ devops/bin/gen_doc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+echo "======== $0 ========"
 set -x
 set -o errexit
 set -o pipefail
 set -o errtrace
 
-OUTPUTDIR=./documentation
 
 err_report() {
     echo "Error running '$1' [rc=$2] line $3 "
@@ -13,14 +13,27 @@ err_report() {
 
 trap 'err_report "$BASH_COMMAND" $? $LINENO' ERR
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DIR="$(pwd)"
 MODULE_DIR="$HOME/.ansible/plugins/modules"
+DOC_DIR="$DIR/documentation"
 
-echo "DIR=$DIR"
-
+# place the modules in the appropriate folder
 [[ ! -d $MODULE_DIR ]] && mkdir -p $MODULE_DIR
+cp $DIR/library/*.py $MODULE_DIR/
 
-cp library/emgr.py $MODULE_DIR
-ansible-doc -t module emgr
+# generate the documentation
+[[ ! -d $DOC_DIR ]] && mkdir -p $DOC_DIR
+set +e
+rc=0
+for f in $DIR/library/*.py; do
+    f="${f##*/}"
+    echo "-------- ansible-doc for $f --------"
+    ansible-doc -t module ${f%%.py} >$DOC_DIR/${f%%.py}.txt
+    rc=$(($rc + $?))
+done
+set -e
+
+# check if we can push
 
 
+exit $rc
