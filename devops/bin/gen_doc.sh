9d8ffcc9440b0a814a7f6b7f4d68123db2133af5
Vianney Robin <vianney.robin@atos.net>
2020-05-18 19:46:58 +0200

    travis: generate the index.html file in gen_doc.sh

--- devops/bin/gen_doc.sh
+++ devops/bin/gen_doc.sh
@@ -33,4 +33,34 @@ for f in $DIR/library/*.py; do
 done
 set -e
 
+echo "Building index.html with TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG"
+cat > $DOC_DIR/index.html <<- _EOF_
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Ansible for power AIX</title>
+  </head>
+  <body>
+    <h1>Ansible for power AIX</h1>
+    <h2>Modules documentation</h2>
+    <ul id="doc_list">
+    </ul>
+    <script>
+      (async () => {
+        const response = await fetch('https://api.github.com/repos/$TRAVIS_REPO_SLUG/contents?ref=gh-pages');
+        const data = await response.json();
+        let htmlString = '<ul>';
+        for (let file of data) {
+          if (file.path == 'index.html') continue;
+          const fileName = file.path.replace('.txt', '');
+          htmlString += \`<li><a href="\${file.path}">\${fileName}</a></li>\`;
+        }
+        htmlString += '</ul>';
+        document.getElementById('doc_list').innerHTML = htmlString;
+      })()
+    </script>
+  <body>
+</html>
+_EOF_
+
 exit $rc
