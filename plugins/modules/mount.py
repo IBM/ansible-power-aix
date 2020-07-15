72f33aa8a787c2ab57fc07c7c19c27b527467f11
kavanabhat <kavana.bhat@in.ibm.com>
2020-07-15 16:15:19 +0530

    Include unmount functionality as part of this module

--- plugins/modules/mount.py
+++ plugins/modules/mount.py
@@ -16,23 +16,43 @@ DOCUMENTATION = r'''
 author:
 - AIX Development Team (@pbfinley1911)
 module: mount
-short_description: Makes a file system available for use
+short_description: Mounts/Unmounts a Filesystem/Device
 description:
-- This module makes a file system available for use at a specified location.
-- Builds other file trees made up of directory and file mounts.
+- This module mounts/unmounts a Filesystem/Device on the specified path
 version_added: '2.9'
 requirements:
 - AIX >= 7.1 TL3
 - Python >= 2.7
 options:
+  state:
+    description:
+    - Specifies the module to perform mount/unmount operation
+    type: str
+    default: mount
+    choices: [mount, umount]
+  node:
+    description:
+    - Specifies the node holding the mounted directory you want to mount/unmount.
   mount_all:
     description:
-    - Mounts all file systems in the /etc/filesystems file with stanzas that contain the true mount attribute
+    - When I(state=mount) and I(mount_all=all), mounts all file systems in the
+      /etc/filesystems file with stanzas that contain the true mount attribute
+    - When I(state=umount) and I(mount_all=all), unmounts all mounted filesystem
+    - When I(state=umount) and I(mount_all=remote), unmounts all remote mounted
+      filesystems
+    type: str
+    choices: [all, remote, none]
+    default: none
+  force:
+    description:
+    - For remote mounted file systems, this attribute forces an unmount to free a
+      client when the server is down and server path names cannot be resolved, or
+      when a file system must be unmounted while it is still in use.
     type: bool
-    default: no
+    default: false
   alternate_fs:
     description:
-    - Mounts on a file of an alternate file system, other than the /etc/file systems file
+    - Mounts on a file of an alternate file system, other than the /etc/filesystems file
     type: str
   removable_fs:
     description:
@@ -46,7 +66,8 @@ options:
     default: no
   fs_type:
     description:
-    - Mounts all stanzas in the /etc/filesystems file that contain the type=fs_type attribute
+    - Mounts/unmounts all stanzas in the /etc/filesystems file that contain the type=fs_type
+      attribute
     type: str
   vfsname:
     description:
@@ -54,11 +75,12 @@ options:
     type: str
   options:
     description:
-    - Specifies options. Options should be of the form <option-name>=<value> and multiple options should be separated only by a comma.
+    - Specifies options. Options should be of the form <option-name>=<value> and multiple
+      options should be separated only by a comma.
     type: str
   mount_dir:
     description:
-    - Directory path to be mounted.
+    - Directory path to be mounted/unmounted
     type: str
   mount_over_dir:
     description:
@@ -67,10 +89,21 @@ options:
 '''
 
 EXAMPLES = r'''
-- name: Specify the filesystems to be mounted
+- name: Mount filesystems
   mount:
-    mount_dir=/dev/hd1
-    mount_over_dir=/home
+    mount_dir: /dev/hd1
+    mount_over_dir: /home
+
+- name: Mount filesystems provided by a node"
+  mount:
+    node: aixbase.aus.stglabs.ibm.com
+    mount_dir: /mnt
+
+- name: Unmount remote filesystems
+  mount:
+   state: umount
+   mount_all: remote
+   force: True
 '''
 
 RETURN = r'''
@@ -78,37 +111,109 @@ msg:
     description: The execution message.
     returned: always
     type: str
-    sample: "Command 'mount myfs' failed with return code 1"
+rc:
+    description: The return code.
+    returned: If the command failed.
+    type: int
 stdout:
     description: The standard output.
-    returned: always
+    returned: If the command failed.
     type: str
-    sample: '  node       mounted        mounted over    vfs       date        options      \n
-             -------- ---------------  ---------------  ------ ------------ --------------- \n
-                      /dev/hd4         /                jfs2   Jun 09 04:37 rw,log=/dev/hd8 \n
-                      /dev/hd2         /usr             jfs2   Jun 09 04:37 rw,log=/dev/hd8 \n
-                      /dev/hd9var      /var             jfs2   Jun 09 04:37 rw,log=/dev/hd8 \n
-                      /dev/hd3         /tmp             jfs2   Jun 09 04:37 rw,log=/dev/hd8 \n
-                      /dev/hd1         /home            jfs2   Jun 09 04:38 rw,log=/dev/hd8 \n
-                      /dev/hd11admin   /admin           jfs2   Jun 09 04:38 rw,log=/dev/hd8 \n
-                      /proc            /proc            procfs Jun 09 04:38 rw              \n
-                      /dev/hd10opt     /opt             jfs2   Jun 09 04:38 rw,log=/dev/hd8 \n
-                      /dev/fslv01      /tftpboot        jfs2   Jun 09 04:38 rw,log=/dev/hd8 \n'
 stderr:
     description: The standard error.
-    returned: always
+    returned: If the command failed.
     type: str
-    sample: 'mount: myfs is not a known file system'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+
+
+def mount(module):
+    """
+    Mount the specified device/filesystem
+    param module: Ansible module argument spec.
+    return: changed - True/False(device/filesystem state modified or not),
+            msg - message
+    """
+    cmd = "mount "
+    alternate_fs = module.params['alternate_fs']
+    if alternate_fs:
+        cmd += "-F %s " % alternate_fs
+    if module.params['removable_fs']:
+        cmd += "-p "
+    if module.params['read_only']:
+        cmd += "-r "
+    vfsname = module.params['vfsname']
+    if vfsname:
+        cmd += "-v %s " % vfsname
+    options = module.params['options']
+    if options:
+        cmd += "-o %s " % options
+    fs_type = module.params['fs_type']
+    node = module.params['node']
+    if node:
+        cmd += "-n %s " % node
+    if fs_type:
+        cmd += "-t %s " % fs_type
+    elif module.params['mount_all'] == 'all':
+        cmd += "all"
+    elif module.params['mount_dir']:
+        mount_dir = module.params['mount_dir']
+        mount_over_dir = module.params['mount_over_dir']
+        if mount_over_dir is None:
+            mount_over_dir = ""
+        cmd += "%s %s" % (mount_dir, mount_over_dir)
+
+    rc, stdout, stderr = module.run_command(cmd)
+    if rc != 0:
+        msg = "Mount failed. Command '%s' failed with return code '%s'." % (cmd, rc)
+        module.fail_json(msg=msg, stdout=stdout, stderr=stderr)
+
+    msg = "Mount Successful. Command '%s' successful." % cmd
+    return True, msg
+
+
+def umount(module):
+    """
+    Unmount the specified device/filesystem
+    param module: Ansible module argument spec.
+    return: changed - True/False(device/filesystem state modified or not),
+            msg - message
+    """
+    mount_all = module.params['mount_all']
+    fs_type = module.params['fs_type']
+    mount_dir = module.params['mount_dir']
+    node = module.params['node']
+    force = module.params['force']
+
+    cmd = "umount "
+    if force:
+        cmd += "-f "
+    if mount_all == 'remote':
+        cmd += "allr "
+    elif mount_all == 'all':
+        cmd += "all "
+    if node:
+        cmd += "-n %s " % node
+    if fs_type:
+        cmd += "-t %s " % fs_type
+    if mount_dir:
+        cmd += mount_dir
+
+    rc, stdout, stderr = module.run_command(cmd)
+    if rc != 0:
+        msg = "Unmount failed. Command '%s' failed with return code '%s'." % (cmd, rc)
+        module.fail_json(msg=msg, stdout=stdout, stderr=stderr)
+
+    msg = "Unmount Successful. Command '%s' successful." % cmd
+    return True, msg
 
 
 def main():
     module = AnsibleModule(
         supports_check_mode=False,
         argument_spec=dict(
-            mount_all=dict(type='bool'),
+            mount_all=dict(type='str', default='none', choices=['all', 'remote', 'none']),
             alternate_fs=dict(type='str'),
             removable_fs=dict(type='bool'),
             read_only=dict(type='bool'),
@@ -117,56 +222,24 @@ def main():
             options=dict(type='str'),
             mount_dir=dict(type='str'),
             mount_over_dir=dict(type='str'),
+            state=dict(type='str', default='mount', choices=['mount', 'umount']),
+            node=dict(type='str'),
+            force=dict(type='bool', default=False),
         ),
 
-        required_together=[["mount_dir", "mount_over_dir"]],
         mutually_exclusive=[
             ["mount_all", "fs_type", "mount_dir"]
         ]
     )
 
-    result = dict(
-        changed=False,
-        msg='',
-        stdout='',
-        stderr='',
-    )
+    state = module.params['state']
 
-    cmd = ['mount']
-    alternate_fs = module.params['alternate_fs']
-    if alternate_fs:
-        cmd += ['-F', alternate_fs]
-    if module.params['removable_fs']:
-        cmd += ['-p']
-    if module.params['read_only']:
-        cmd += ['-r']
-    vfsname = module.params['vfsname']
-    if vfsname:
-        cmd += ['-v', vfsname]
-    options = module.params['options']
-    if options:
-        cmd += ['-o', options]
-    fs_type = module.params['fs_type']
-    if module.params['fs_type']:
-        cmd += ['-t', fs_type]
-    elif module.params['mount_all']:
-        cmd += ['all']
-    elif module.params['mount_dir']:
-        mount_dir = module.params['mount_dir']
-        mount_over_dir = module.params['mount_over_dir']
-        cmd += [mount_dir, mount_over_dir]
+    if state == 'mount':
+        changed, msg = mount(module)
+    else:
+        changed, msg = umount(module)
 
-    rc, stdout, stderr = module.run_command(cmd)
-
-    result['stdout'] = stdout
-    result['stderr'] = stderr
-    if rc != 0:
-        result['msg'] = 'Command \'{0}\' failed with return code {1}.'.format(' '.join(cmd), rc)
-        module.fail_json(**result)
-
-    result['msg'] = 'Command \'{0}\' successful.'.format(' '.join(cmd))
-    result['changed'] = True
-    module.exit_json(**result)
+    module.exit_json(changed=changed, msg=msg)
 
 
 if __name__ == '__main__':
