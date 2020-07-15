2b461b59d455de2da268dfbcebc68dd7a7f6b32c
dberg1 <dberg1@github.com>
2020-07-15 15:36:09 +0200

    vios_alt_disk: improve documentation

--- plugins/modules/vios_alt_disk.py
+++ plugins/modules/vios_alt_disk.py
@@ -56,10 +56,8 @@ options:
 notes:
   - C(alt_disk_copy) only backs up mounted file systems. Mount all file
     systems that you want to back up.
-  - copy is performed only on one alternate hdisk even if the rootvg
-    contains multiple hdisks
-  - error if several C(altinst_rootvg) exist for cleanup operation in
-    automatic mode
+  - when no target is specified, copy is performed to only one alternate
+    disk even if the rootvg contains multiple disks
 '''
 
 EXAMPLES = r'''
