fbd6db47ed7cb17848a3aa5c054fb946ced2bb3c
Vianney Robin <vianney.robin@atos.net>
2020-07-16 15:44:58 +0200

    sanity_test: voluptuous pycodestyle should be installed in venv

--- devops/bin/sanity_test.sh
+++ devops/bin/sanity_test.sh
@@ -18,7 +18,7 @@ trap 'err_report "$BASH_COMMAND" $? $LINENO' ERR
 DIR="$(pwd)"
 
 # build the virtual environment to run ansible-test
-cd $ANSIBLE_DIR
+cd
 git clone https://github.com/ansible/ansible.git
 cd ansible
 ANSIBLE_DIR="$(pwd)"
@@ -29,6 +29,7 @@ python3 -m venv venv
 pip install -r requirements.txt
 . hacking/env-setup
 pip install -r docs/docsite/requirements.txt
+pip3 install voluptuous pycodestyle
 [[ -e $(find test/ -name sanity.txt) ]] && pip install -r $(find test/ -name sanity.txt)
 
 # place the modules in the appropriate folder
