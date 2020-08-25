3cac95643433eff825435abca2f3e3d35be3e9ac
Vianney Robin <vianney.robin@atos.net>
2020-08-25 18:01:02 +0200

    backup: add mksysb backup creation

--- plugins/modules/backup.py
+++ plugins/modules/backup.py
@@ -21,7 +21,7 @@ description:
 - This module manages backup image of data or system volume group.
 - It uses mksysb or savevg command to create the backup image of the volume group either in a file
   or onto a device.
-- It uses restvg or alt_disk_mksysb to restore a backup image to a disk.
+- It uses restvg or alt_disk_mksysb to restore a backup image to disk(s).
 version_added: '2.9'
 requirements:
 - AIX >= 7.1 TL3
@@ -32,56 +32,64 @@ options:
     - Specifies the operation to perform on the LPAR.
     - C(create) to create a backup image.
     - C(restore) to restore a backup image.
-    - C(view) to get useful information about a backup image.
+    - C(view) to get useful information about a backup image; can be used only for I(type=savevg)
     type: str
     choices: [ create, restore, view ]
     required: true
-  os_backup:
+  type:
     description:
-    - Specifies the operation concerns the Operating system.
-    - If I(yes), it will use mksysb to create an Operating System backup of the root volume group
-      and alt_disk_mksysb to restore the backup image onto a disk.
-    - If I(no), it will use savevg to list and back up all files belonging to a volume group and
-      restvg to restore a backup image onto a disk.
-    type: bool
-    default: no
+    - Specifies the type of backup object to operate.
+    - C(mksysb) is under construction.
+    - C(mksysb) operates on backup of the operating system (that is, the root volume group) of a LPAR or VIOS target.
+    - C(savevg) operates on LPAR savevg, that is all files belonging to a volume group.
+    - Discarded for I(action=view) as this action only applies to savevg.
+    type: str
+    choices: [ mksysb, savevg ]
+    default: savevg
   name:
     description:
     - Specifies the targets of the operation.
-    - Required if C(action=create) and C(os_backup=no), then it specifies the volume group to back up.
-    - If C(action=restore), it specifies the disk device to restore to and do not use the one in
+    - Discarded if I(type=mksysb).
+    - Required if I(action=create) and I(type=savevg), then it specifies the volume group to back up.
+    - If I(action=restore) and I(type=savevg), it specifies the disk device to restore to and do not use the one in
       the vgname.data file.
     type: str
   flags:
     description:
     - Specifies additional flag to pass to the command. Refers to IBM documentation for details.
-    - For C(action=create) and C(os_backup=no), you could use I(-a -A -b Blocks -p -T -V -Z).
-    - For C(action=restore) and C(os_backup=no), you could use I(-b Blocks -n -P PPsize).
+    - For I(action=create) and I(type=savevg), you could use C(-a -A -b Blocks -p -T -V -Z).
+    - For I(action=restore) and I(type=savevg), you could use C(-b Blocks -n -P PPsize).
     type: str
   location:
     description:
     - Specifies the location of the backup files. It can be a device or file path.
-    - When C(os_backup=no), the default device is I(/dev/rmt0).
-    - Can be used if C(os_backup=no). Required if C(action=view).
+    - When I(type=savevg), the default device is C(/dev/rmt0).
+    - Required if I(action=view) or I(type=mksysb).
     type: path
+  disk:
+    description:
+    - Specifies the name or names of the target disks where the alternate rootvg is created.
+    - This disk or these disks must not currently contain any volume group definition.
+    - Required if I(action=restore) and I(type=mksysb).
+    type: list
+    elements: str
   data_file:
     description:
     - Specifies a filename to be used as the vgname.data file instead of the one contained within
       the backup image being restored.
     - The filename can be specified by either a relative or an absolute pathname.
-    - Can be used if C(os_backup=no) and C(action=restore).
+    - Can be used if I(type=savevg) and I(action=restore).
     type: path
   create_data_file:
     description:
-    - Specifies to create the data file containing information including the list of logical volumes
-      file systems and their sizes, and the volume group name.
-    - When C(volume_group) is the rootvg, data file will be in I(/image.data).
-    - When C(volume_group) is a data volume group, data file will be in
-      I(/tmp/vgdata/vgname/vgname.data).
-    - Specify C(create_data_file=mapfile) to creates the MAPFILE containing the mapping of the logical
+    - Specifies to create the data file containing information on the vloume group, logical volumes,
+      file systems and their sizes.
+    - If I(volume_group=rootvg), then data file will be in C(/image.data).
+    - If C(volume_group) is a data volume group, data file will be in C(/tmp/vgdata/vgname/vgname.data).
+    - Specify I(create_data_file=mapfile) to creates the MAPFILE containing the mapping of the logical
       to physical partitions for each logical volume in the volume group. This mapping can be used
       to allocate the same logical-to-physical mapping when the image is restored.
-    - Can be used if C(action=create) and C(os_backup=no).
+    - Can be used if I(action=create).
     type: str
     choices: [ yes, mapfile, no ]
     default: no
@@ -89,22 +97,29 @@ options:
     description:
     - Specifies a file path that contains the list of file systems to exclude from the backup.
     - One file system mount point is listed per line.
-    - Can be used if C(action=create) and C(os_backup=no).
+    - Can be used if I(action=create).
     type: path
   force:
     description:
     - Specifies to overwrite existing backup image.
-    - Can be used if C(os_backup=no) and C(action=create).
+    - Can be used if I(action=create) and I(type=savevg).
     type: bool
     default: no
   exclude_data:
     description:
-    - If C(action=create), specifies to exclude user data from the backup. Backs up user volume
+    - If I(action=create), specifies to exclude user data from the backup. Backs up user volume
       group information and administration data files. This backs up files such as
       /tmp/vgdata/vgname/vgname.data and map files if any exist. Cannot be used on a rootvg.
-    - If C(action=restore), specifies to recreate the volume group structure only without restoring
+    - If I(action=restore), specifies to recreate the volume group structure only without restoring
       any files or data.
-    - Can be used if C(os_backup=no) and C(action=create) or C(action=restore).
+    - Can be used if I(type=savevg).
+    type: bool
+    default: no
+  exclude_packing_files:
+    description:
+    - Specifies to exclude files specified in the /etc/exclude_packing.rootvg,
+      /etc/exclude_packing.vgname, or /etc/exclude_packing.WPARname file from being packed.
+    - Can be used if I(action=create) and I(type=mksysb).
     type: bool
     default: no
   exclude_files:
@@ -113,26 +128,26 @@ options:
     - The /etc/exclude.vgname file should contain patterns of file names that you do not want
       included in your system backup image. They are input to the pattern matching conventions of
       the grep command.
-    - Can be used if C(action=create) and C(os_backup=no).
+    - Can be used if I(action=create).
     type: bool
     default: no
   extend_fs:
     description:
     - Specifies to extend the filesystem if needed.
-    - Can be used if C(action=create) and C(os_backup=no).
+    - Can be used if I(action=create) and I(type=savevg).
     type: bool
     default: no
   minimize_lv_size:
     description:
     - Specifies to create the logical volumes with minimum size possible to accomodate
       the file system.
-    - Can be used if C(action=restore) and C(os_backup=no).
+    - Can be used if I(action=restore) and I(type=savevg).
     type: bool
     default: no
   verbose:
     description:
     - Specifies to run the operation in verbose mode.
-    - Can be used if C(action=create) or C(action=restore).
+    - Can be used if I(action=create) or I(action=restore).
     type: bool
     default: no
 notes:
@@ -140,13 +155,14 @@ notes:
   - C(savevg) backs up all logical volume information and will be recreated. However, only
     JFS-mounted file system data will be backed up. Raw logical volume data will NOT be backed up
     using a savevg.
+  - C(savevg) only backs up varied-on volume group. The file systems must be mounted.
 '''
 
 EXAMPLES = r'''
 - name: savevg of rootvg to /dev/hdisk1
   backup:
     action: create
-    os_backup: no
+    type: savevg
     name: rootvg
     location: /dav/rmt1
     exclude_data: no
@@ -159,7 +175,7 @@ EXAMPLES = r'''
 - name: savevg of datavg structure to /dev/hdisk2
   backup:
     action: create
-    os_backup: no
+    type: savevg
     name: datavg
     location: /tmp/backup_datavg
     exclude_data: yes
@@ -169,13 +185,13 @@ EXAMPLES = r'''
 - name: view the vg backup image stored on /dev/hdisk1 with savevg
   backup:
     action: view
-    os_backup: no
+    type: savevg
     location: /dev/hdisk1
 
 - name: restvg to restore datavg structure only to /dev/hdisk2
   backup:
     action: restore
-    os_backup: no
+    type: savevg
     name: datavg
     location: /tmp/backup_datavg
     data_file: /tmp/datavg.mydata
@@ -271,26 +287,62 @@ def mksysb(module, params):
     Perform a mksysb of the OS.
 
     arguments:
-        module      (dict): The Ansible module
+        module  (dict): The Ansible module
         params  (dict): the command parameters
     return:
-        True if backup succeeded
-        False otherwise
+        rc       (int): the return code of the command
     """
     global results
     module.log('Creating OS backup with mksysb.')
 
     # mksysb  device | file
     # not yet implemented:
-    # [ -a ] [ -A ] [ -b number ] [ -e ] [ -F filename ] [ -i ] [ -m ] [ -p ] [ -P ]
-    # [ -t argument ] [ -v ] [ -V ] [-x file ] [ -X ] [-Z] [ -G | -N ] [-M] [ -T ]
-    cmd = ['/bin/mksysb']
+    # [ -e ]          Excludes files specified in the /etc/exclude.vgname file from being backed up.
+    # [ -i | -m ]     Create the image.data file calling mkszfile to get VG, LV, PV, PS info (-m for map file).
+    # [ -P ]          Excludes files that are listed line by line in predifined files from being packed.
+    # [ -v ]          Verbose mode.
+    # [-x file]       Exclude fs listed in the file (one per line).
+    # [ -X ]          Extend fs if needed.
+    # not yet implemented:
+    # [ -a ]          Does not back up extended attributes or NFSv4 ACLs.
+    # [ -A ]          Backs up DMAPI file system files.
+    # [ -b number ]   Specifies the number of 512-byte blocks to write in a single output operation.
+    # [ -F filename ] Specifies a previously created mksysb from which a backup tape is created to try to make the tape bootable.
+    # [ -G | -N ]     Excludes|Includes WPAR file systems from|to the system backup.
+    # [-M]            Creates a backup file that is intended for use with the multibos command.
+    # [ -p ]          Disable software packing (tape drives).
+    # [ -t path ]     Directory or file system to create a boot image from the mksysb file specified by the -F flag.
+    # [ -T ]          Create backup using snapshots.
+    # [ -V ]          Verify a tape backup.
+    # [ -Z ]          Does not back up the EFS information for all the files, directories, and file systems.
 
+    cmd = ['/bin/mksysb']
+    if params['create_data_file'] == 'mapfile':
+        cmd += ['-m']
+    elif params['create_data_file'].lower() == 'yes':
+        cmd += ['-i']
+    if params['exclude_packing_files']:
+        cmd += ['-P']
+    if params['exclude_files']:
+        cmd += ['-e']
+    if params['verbose']:
+        cmd += ['-v']
+    if params['exclude_fs']:
+        cmd += ['-x', params['exclude_fs']]
+    if params['extend_fs']:
+        cmd += ['-X']
     if params['flags']:
         for f in params['flags'].split(' '):
             cmd += [f]
+    cmd += [params['location']]
 
-    return True
+    rc, stdout, stderr = module.run_command(cmd)
+    results['stdout'] = stdout
+    results['stderr'] = stderr
+    results['rc'] = rc
+    if rc == 0:
+        results['changed'] = True
+    return rc
 
 
 def alt_disk_mksysb(module, params):
@@ -379,9 +431,9 @@ def savevg(module, params, vg):
         cmd += ['-e']
     if params['location']:
         cmd += ['-f', params['location']]
-    if params['create_data_file'] == 'mapfile':
+    if params['create_data_file'].lower() == 'mapfile':
         cmd += ['-m']
-    elif params['create_data_file']:
+    elif params['create_data_file'].lower() == 'yes':
         cmd += ['-i']
     if params['exclude_data']:
         cmd += ['-r']
@@ -489,14 +541,20 @@ def main():
     global results
 
     module = AnsibleModule(
+        supports_check_mode=False,
         argument_spec=dict(
             action=dict(required=True, type='str',
                         choices=['create', 'restore', 'view']),
-            os_backup=dict(type='bool', default=False),
+            type=dict(type='str', choices=['mksysb', 'savevg'], default='savevg'),
             name=dict(type='str'),
             flags=dict(type='str'),
-            # for savevg and restvg
+            # for mksysb
+            exclude_packing_files=dict(type='bool', default=False),
+            # for alt_disk_mksysb
+            disk=dict(type='list', elements='str'),
+            # for alt_disk_mksysb and savevg, restvg
             location=dict(type='path'),
+            # for savevg and restvg
             exclude_data=dict(type='bool', default=False),
             verbose=dict(type='bool', default=False),
             # for savevg
@@ -511,6 +569,7 @@ def main():
         ),
         required_if=[
             ['action', 'view', ['location']],
+            ['type', 'mksysb', ['location']],
         ],
     )
 
@@ -524,24 +583,26 @@ def main():
 
     params = {}
     action = module.params['action']
-    params['os_backup'] = module.params['os_backup']
+    params['objtype'] = module.params['objtype']
     params['flags'] = module.params['flags']
+    params['location'] = module.params['location']
 
     if action == 'create':
-        if params['os_backup']:
+        params['verbose'] = module.params['verbose']
+        params['create_data_file'] = module.params['create_data_file']
+        params['exclude_fs'] = module.params['exclude_fs']
+        params['exclude_files'] = module.params['exclude_files']
+        params['extend_fs'] = module.params['extend_fs']
+
+        if params['objtype'] == 'mksysb':
+            params['exclude_packing_files'] = module.params['exclude_packing_files']
 
             rc = mksysb(module, params)
 
-        else:
+        elif params['objtype'] == 'savevg':
             params['name'] = module.params['name']
-            params['location'] = module.params['location']
             params['exclude_data'] = module.params['exclude_data']
-            params['verbose'] = module.params['verbose']
-            params['create_data_file'] = module.params['create_data_file']
-            params['exclude_fs'] = module.params['exclude_fs']
             params['force'] = module.params['force']
-            params['exclude_files'] = module.params['exclude_files']
-            params['extend_fs'] = module.params['extend_fs']
 
             if not params['name']:
                 results['msg'] = 'Missing parameter: action is {0} but argument \'name\' is missing.'.format(action)
@@ -553,13 +614,17 @@ def main():
             rc = savevg(module, params, params['name'])
 
     elif action == 'restore':
-        if params['os_backup']:
+        if params['objtype'] == mksysb:
+            params['disk'] = module.params['disk']
+
+            if not params['disk']:
+                results['msg'] = 'Missing parameter: action is {0} but argument \'disk\' is missing.'.format(action)
+                module.fail_json(**results)
 
             rc = alt_disk_mksysb(module, params)
 
-        else:
+        elif params['objtype'] == savevg:
             params['name'] = module.params['name']
-            params['location'] = module.params['location']
             params['exclude_data'] = module.params['exclude_data']
             params['verbose'] = module.params['verbose']
             params['data_file'] = module.params['data_file']
@@ -568,8 +633,8 @@ def main():
             rc = restvg(module, params, action, params['name'])
 
     elif action == 'view':
-        if params['os_backup']:
-            results['msg'] = 'Bad parameter: action is {0}, os_backup cannot be \'{1}\'.'.format(action, params['os_backup'])
+        if params['objtype'] != savevg:
+            results['msg'] = 'Bad parameter: action is {0}, objtype cannot be \'{1}\'.'.format(action, params['objtype'])
             module.fail_json(**results)
 
         params['location'] = module.params['location']
