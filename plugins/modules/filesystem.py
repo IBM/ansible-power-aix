fdc3b859bf7df7cb2e22a6121425e6dd7bd56ee6
Joseph de Joya <Joseph.de.Joya@ibm.com>
2021-04-30 17:58:21 -0500

    remove extra space

--- plugins/modules/filesystem.py
+++ plugins/modules/filesystem.py
@@ -7,7 +7,6 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
@@ -263,7 +262,6 @@ def check_attr_change(module, filesystem):
         if new_amount != old_amount:
             return True
 
-
     # check in account subsystem changed
     new_acct_sub_sys = module.params["account_subsystem"]
     if new_acct_sub_sys is not None:
@@ -334,7 +332,6 @@ def chfs(module, filesystem):
     return: changed - True/False(filesystem state modified or not),
             msg - message
     """
-    
     attrs = module.params["attributes"]
     acct_sub_sys = module.params["account_subsystem"]
     amount = module.params["auto_mount"]
@@ -367,7 +364,6 @@ def chfs(module, filesystem):
         cmd = "chnfsmnt %s -f %s -d %s -h %s" % (opts, filesystem, device, nfs_server)
 
     else:
-        
         # Modify Local Filesystem
         if amount is True:
             opts += "-A yes "
