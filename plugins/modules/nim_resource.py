9bcdcd5ede45c06e58f8863485e87516e3eae8c8
Pedro Torres <pvtorres@us.ibm.com>
2022-03-03 16:20:11 -0600

    Prototype new module nim_resource to allow the define/remove/display … (#45)
    - first availability of nim_resource module to allow the define/remove/display of NIM resources.
    - idempotency aware by reading commands errors.
    - allows the creation of resources of multiple types.
    - It does not verify mismatch input and lets the command to fail.
    - display all defined objects or can display a specific one.
    - added demo playbook for nim_resource module
    - changes for source, location to be attributes in the playbook.
    - display results of **show** action in a dictionary: nim_resources
    - add nim_resource_found field in results to easily check if the resource was found.
    - add support for check_mode (final command preview execution only)
    - add clarification in the documentation that not all the attributes in the
      playbook are required.
    - add examples on how to create resource groups.
    * Documentation Notes:
    - explain in the non required parameters, that they are needed for certain
    action states and optional for others.
    - add more explanation on the choices for object_type.
    
    Signed-off-by: Joseph de Joya <Joseph.de.Joya@ibm.com>

---
+++ plugins/modules/nim_resource.py
@@ -0,0 +1,422 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2022- IBM, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r'''
+---
+author:
+- AIX Development Team (@pvtorres1703)
+module: nim_resource
+short_description: show/define/delete NIM resource object(s).
+description:
+- This module facilitates the display, creation removal or  of NIM resource objects.
+requirements:
+- AIX >= 7.1 TL3
+- Python >= 2.7
+- User with root authority to run the nim command.
+- NIM master software bos.sysmgt.nim.master.
+- 'Privileged user with authorization:
+  B(aix.system.install,aix.system.nim.config.server,aix.system.nim.stat)'
+options:
+  action:
+    description:
+    - Specifies the action to be performed.
+    - C(show) shows all NIM resource objects. Can be used with options I(name) or I(object_type) to filter objects.
+    - C(create) creates a NIM resource object. It requires options I(name), I(object_type) and I(attributes).
+    - C(delete) deletes a NIM resource object. It requires option I(name).
+    type: str
+    choices: [ show, create, delete ]
+    required: true
+  name:
+    description:
+    - Specifies the NIM object name.
+    type: str
+    required: false
+  object_type:
+    description:
+    - NIM resource object's type.
+    - Required for action I(action=create).
+    - Optional for I(action=show).
+    - Some of the choices are provided as examples. For details of all possible
+      NIM resources objects, refer to the IBM documentation at
+      U(https://www.ibm.com/docs/en/aix/7.2?topic=management-using-nim-resources)
+    - The following are examples of the most common choices "pp_source, spot, bosinst_data, mksysb, fb_script and  res_group"
+    type: str
+    required: false
+  attributes:
+    description:
+    - Specifies the attribute-value pairs required for I(action=create) or I(action=show)
+    type: dict
+    required: false
+notes:
+  - You can refer to the IBM documentation for additional information on the NIM concept and command
+    at U(https://www.ibm.com/support/knowledgecenter/ssw_aix_73/install/nim_concepts.html),
+    U(https://www.ibm.com/support/knowledgecenter/ssw_aix_73/n_commands/nim.html),
+    U(https://www.ibm.com/support/knowledgecenter/ssw_aix_73/n_commands/nim_master_setup.html).
+'''
+
+EXAMPLES = r'''
+- name: Show all NIM resource objects.
+  ibm.power_aix.nim_resource:
+    action: show
+
+- name: Create a copy of the images from source to location and
+        define a NIM lpp_source resource from that location.
+  ibm.power_aix.nim_resource:
+    action: create
+    name: lpp_730
+    object_type: lpp_resource
+    attributes:
+      source: /software/AIX7300
+      location: /nim1/copy_AIX7300_resource
+
+- name: Define a NIM lpp_source resource object from a directory that
+        contains the images.
+  ibm.power_aix.nim_resource:
+    action: create
+    name: lpp_730
+    object_type: lpp_resource
+    attributes:
+      location: /nim1/copy_AIX7300_resource
+
+- name: Define a NIM spot (Shared Product Object Tree) resource
+        using a defined lpp_source.
+  ibm.power_aix.nim_resource:
+    action: create
+    name: spot_730
+    object_type: spot
+    attributes:
+      source: lpp_730
+      location: /nim1/spot_730_resource
+
+- name: Create a NIM resource group object.
+  ibm.power_aix.nim_resource:
+    action: create
+    name: ResGrp730
+    object_type: res_group
+    attributes:
+      lpp_source: lpp_730
+      spot: spot_730
+      bosinst_data: bosinst_data730
+      comments: "730 Resources"
+
+- name: Show all the defined NIM resource objects.
+  ibm.power_aix.nim_resource:
+    action: show
+
+- name: Show all the defined NIM resource objects of type spot.
+  ibm.power_aix.nim_resource:
+    action: show
+    object_type: spot
+
+- name: Show a specific NIM resource object.
+  ibm.power_aix.nim_resource:
+    action: show
+    name: lpp_730
+
+- name: Delete a NIM resource object.
+  ibm.power_aix.nim_resource:
+    action: delete
+    name: spot_730
+'''
+
+RETURN = r'''
+msg:
+    description: The execution message.
+    returned: always
+    type: str
+    sample: 'Resource spot_730 was removed.'
+rc:
+    description: The return code.
+    returned: If the command failed.
+    type: int
+stdout:
+    description: The standard output.
+    returned: always
+    type: str
+stderr:
+    description: The standard error.
+    returned: always
+    type: str
+cmd:
+    description: Command executed.
+    returned: always
+    type: str
+nim_resource_found:
+    description: Return if a queried object resource exist.
+    returned: If I(action=show).
+    type: bool
+nim_resources:
+    description: Dictionary output with the NIM resource object information.
+    returned: If I(action=show).
+    type: dict
+    sample:
+        "nim_resources": {
+            "lpp_source_test": {
+                "Rstate": "ready for use",
+                "alloc_count": "0",
+                "arch": "power",
+                "class": "resources",
+                "location": "/nim1/lpp_source_test4",
+                "prev_state": "unavailable for use",
+                "server": "master",
+                "simages": "yes",
+                "type": "lpp_source",
+            }
+        }
+
+'''
+
+import re
+from ansible.module_utils.basic import AnsibleModule
+
+results = None
+
+
+def res_show(module):
+    '''
+    Show nim resources.
+
+    arguments:
+        module  (dict): The Ansible module
+    note:
+        Exits with fail_json in case of error
+    return:
+        updated results dictionary.
+    '''
+
+    cmd = '/usr/sbin/lsnim -l'
+    name = module.params['name']
+    object_type = module.params['object_type']
+
+    # This module will only show general information about the resource
+    # object class.
+    if not object_type and not name:
+        cmd += ' -c resources'
+
+    if object_type:
+        cmd += ' -t {0}'.format(object_type)
+
+    if name:
+        cmd += ' ' + name
+
+    if module.check_mode:
+        results['msg'] = 'Command \'{0}\' preview mode, execution skipped.'.format(cmd)
+        return
+
+    return_code, stdout, stderr = module.run_command(cmd)
+
+    results['stderr'] = stderr
+    results['stdout'] = stdout
+    results['cmd'] = cmd
+    results['nim_resources'] = {}
+    results['nim_resource_found'] = False
+
+    if return_code != 0:
+
+        # 0042-053 The NIM objefct is not there.
+        pattern = r"0042-053"
+        found = re.search(pattern, stderr)
+
+        if found:
+            results['msg'] = 'There is no NIM object resource named {0} '.format(name)
+        else:
+            results['msg'] = 'Error trying to display object {0}'.format(name)
+            results['rc'] = return_code
+            module.fail_json(**results)
+    else:
+        results['nim_resources'] = build_dic(stdout)
+        results['nim_resource_found'] = True
+
+    return
+
+
+def res_create(nim_cmd, module):
+    '''
+    Define a NIM resource object.
+
+    arguments:
+        module  (dict): The Ansible module
+    note:
+        Exits with fail_json in case of error
+    return:
+        updated results dictionary.
+    '''
+
+    cmd = nim_cmd + ' -a server=master -o define '
+    opts = ""
+    name = module.params['name']
+    object_type = module.params['object_type']
+    attributes = module.params['attributes']
+
+    if object_type:
+        cmd += ' -t ' + object_type
+
+    if attributes is not None:
+        for attr, val in attributes.items():
+            opts += " -a {0}=\"{1}\" ".format(attr, val)
+        cmd += opts
+
+    if name:
+        cmd += ' ' + name
+
+    if module.check_mode:
+        results['msg'] = 'Command \'{0}\' preview mode, execution skipped.'.format(cmd)
+        return
+
+    return_code, stdout, stderr = module.run_command(cmd)
+
+    results['stderr'] = stderr
+    results['stdout'] = stdout
+    results['cmd'] = cmd
+
+    if return_code != 0:
+
+        # 0042-081 The resource already exists on "master"
+        pattern = r"0042-081"
+        found = re.search(pattern, stderr)
+        if not found:
+            results['rc'] = return_code
+            results['msg'] = 'Error trying to define resource {0} '.format(name)
+            module.fail_json(**results)
+        else:
+            results['msg'] = 'Resource already exist'
+
+    else:
+        results['msg'] = 'Creation of resource {0} was a success'.format(name)
+        results['changed'] = True
+
+    return
+
+
+def res_delete(nim_cmd, module):
+    '''
+    Remove a NIM resource object.
+
+    arguments:
+        module  (dict): The Ansible module
+    note:
+        Exits with fail_json in case of error
+    return:
+        updated results dictionary.
+    '''
+
+    name = module.params['name']
+    cmd = nim_cmd + ' -o remove {0}'.format(name)
+
+    if module.check_mode:
+        results['msg'] = 'Command \'{0}\' in preview mode, execution skipped.'.format(cmd)
+        return
+
+    return_code, stdout, stderr = module.run_command(cmd)
+
+    results['stderr'] = stderr
+    results['stdout'] = stdout
+    results['cmd'] = cmd
+
+    if return_code != 0:
+
+        # 0042-053 The NIM objefct is not there.
+        pattern = r"0042-053"
+        found = re.search(pattern, stderr)
+
+        if found:
+            results['msg'] = 'There is no NIM object resource named {0} '.format(name)
+        else:
+            results['msg'] = 'Error trying to remove NIM object {0}'.format(name)
+            results['rc'] = return_code
+            module.fail_json(**results)
+
+    else:
+        results['msg'] = 'Resource {0} was removed.'.format(name)
+        results['changed'] = True
+
+    return
+
+
+def build_dic(stdout):
+    """
+    Build dictionary with the stdout info
+
+    arguments:
+        stdout   (str): stdout of the command to parse
+    returns:
+        info    (dict): NIM object dictionary
+    """
+
+    info1 = {}
+    info = {}
+
+    lines = stdout.splitlines()
+
+    for line in lines:
+
+        key = (line.split('=')[0]).strip()
+        size = len(line.split('='))
+
+        if size > 1:
+            value = (line.split('=')[1]).strip()
+            info1[key] = value
+        else:
+            info[key[:-1]] = info1
+            info1.clear()
+
+    return info
+
+
+def main():
+    global results
+    module = AnsibleModule(
+        argument_spec=dict(
+            action=dict(type='str', required=True, choices=['show', 'create', 'delete']),
+            name=dict(type='str'),
+            object_type=dict(type='str'),
+            attributes=dict(type='dict'),
+        ),
+        required_if=[
+            ('action', 'create', ('name', 'object_type', 'attributes')),
+            ('action', 'delete', ('name',), True),
+        ],
+        supports_check_mode=True
+    )
+
+    results = dict(
+        changed=False,
+        msg='',
+        stdout='',
+        stderr='',
+    )
+
+    nim_cmd = module.get_bin_path('nim', required=False)
+
+    if nim_cmd is None:
+        results['msg'] = "Target nim server does not have the required software for "\
+            "nim server operations. Verify if the filesystem bos.sysmgt.nim.master is installed."
+        module.fail_json(**results)
+
+    action = module.params['action']
+
+    if action == 'show':
+        res_show(module)
+    elif action == 'create':
+        res_create(nim_cmd, module)
+    elif action == 'delete':
+        res_delete(nim_cmd, module)
+    else:
+        results['msg'] = 'The action selected is NOT recognized. Please check again.'
+        module.fail_json(**results)
+
+    module.exit_json(**results)
+
+
+if __name__ == '__main__':
+    main()
