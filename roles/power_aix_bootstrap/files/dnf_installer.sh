26ead4f0997c3a6a7f9adef383561a91c491828b
schamola <shreyansh.chamola@ibm.com>
2023-08-27 11:20:29 +0530

    Made the following changes:   Updated dnf_installer.sh script   Added the option of adding proxy in dnf.conf file   Solved issues #327 #326 #325 #304
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- roles/power_aix_bootstrap/files/dnf_installer.sh
+++ roles/power_aix_bootstrap/files/dnf_installer.sh
@@ -16,7 +16,7 @@ else
 fi
 
 # Check if we are running this as the root user.
-if [[ "$(id -u)" != "0" ]]
+if [[ "$(/usr/bin/id -u)" != "0" ]]
 then
     echo "This script must be run as root."
     exit 1
@@ -45,7 +45,7 @@ fi
 
 # Check if yum3 is installed.
 yum3_instd=0
-$RPM_CMD -qa | grep yum-3.4.3 > /dev/null 2>&1
+$RPM_CMD -qa | /usr/bin/grep yum-3.4.3 > /dev/null 2>&1
 if [[ $? -eq 0 ]]
 then
     yum3_instd=1
@@ -77,38 +77,27 @@ fi
 
 # Check openssl version.
 function print_openssl_err {
-    echo "Please install openssl 1.0.2.2001 and higher version."
+    echo "Please install openssl 1.1.x and higher version."
     echo "You can download and install latest openssl from AIX web download site"
     echo "https://www-01.ibm.com/marketing/iwm/platform/mrs/assets?source=aixbp"
     exit 1
 }
 ssl_ver=$(lslpp -Lc openssl.base | /usr/bin/awk 'FNR==2' | /usr/bin/awk -F':' '{print $3}')
-f1=$(echo "$ssl_ver" | /usr/bin/cut -d"." -f1)
-f2=$(echo "$ssl_ver" | /usr/bin/cut -d"." -f2)
-f3=$(echo "$ssl_ver" | /usr/bin/cut -d"." -f3)
-f4=$(echo "$ssl_ver" | /usr/bin/cut -d"." -f4)
-if [[ ( ! $f1 -ge 1 ) || ( ! $f2 -ge 0 ) || ( ! $f3 -ge 0) ]]
-then
-     print_openssl_err
-elif [[ ( $f2 -eq 0 ) && (  $f3 -eq 1) ]]
+f1=$(echo $ssl_ver | /usr/bin/cut -d"." -f1)
+f2=$(echo $ssl_ver | /usr/bin/cut -d"." -f2)
+f3=$(echo $ssl_ver | /usr/bin/cut -d"." -f3)
+f4=$(echo $ssl_ver | /usr/bin/cut -d"." -f4)
+if [[ ( ! $f1 -ge 1 ) ]] || [[ ( $f1 -eq  1 ) &&  ( ! $f2 -ge 1 ) ]]
 then
     print_openssl_err
-else
-    if [[ ( $f1 -eq 1 ) && ($f2 -eq 0 ) && ( $f3 -eq 2 ) ]]
-    then
-        if [[ ( ! $f4 -ge 2001 ) ]]
-        then
-             print_openssl_err
-        fi
-    fi
 fi
 
-oslvl=$(/usr/bin/oslevel)
+oslvl=`/usr/bin/oslevel`
 aix_730_plus=0
-os_f1=$(echo "$oslvl" | /usr/bin/cut -d"." -f1)
-os_f2=$(echo "$oslvl" | /usr/bin/cut -d"." -f2)
-os_f3=$(echo "$oslvl" | /usr/bin/cut -d"." -f3)
-os_f4=$(echo "$oslvl" | /usr/bin/cut -d"." -f4)
+os_f1=$(echo $oslvl | /usr/bin/cut -d"." -f1)
+os_f2=$(echo $oslvl | /usr/bin/cut -d"." -f2)
+os_f3=$(echo $oslvl | /usr/bin/cut -d"." -f3)
+os_f4=$(echo $oslvl | /usr/bin/cut -d"." -f4)
 if [[ ( $os_f1 -ge 7 ) && ( $os_f2 -ge 3 ) && ( $os_f3 -ge 0 ) && ( $os_f4 -ge 0 ) ]]
 then
     aix_730_plus=1
@@ -126,32 +115,30 @@ fi
 
 # Check if /tmp has enough space to download rpm.rte & dnf_bundle
 # and size for extracting rpm packages.
-# For 71_72 bundle it requires around 340 MB of free space.
-# 170M for bundle which includes rpm.rte (40M) and rpm packages (130M).
-# rpm packages extracted.
+
 if [[ $aix_730_plus -eq 1 ]]
 then
-    typeset -i total_req=$(echo "(270)" | bc)
-    tmp_free=$(/usr/bin/df -m "$tmppath" | sed -e /Filesystem/d | awk '{print $3}' | bc)
+    typeset -i total_req=$(echo "(512)" | bc)
+    tmp_free=$(/usr/bin/df -m "$tmppath" | /usr/bin/sed -e /Filesystem/d | /usr/bin/awk '{print $3}')
     if [[ $tmp_free -le $total_req ]]
     then
       chfs -a size=+$(( total_req - tmp_free ))M "$tmppath"
       if [[ $? -ne 0 ]];
       then
-          echo "Please make sure $tmppath has around 270MB of free space to download and"
+          echo "Please make sure $tmppath has around 512MB of free space to download and"
           echo "extract files from dnf_bundle."
           exit 1
       fi
     fi
 else
-    typeset -i total_req=`echo "(340)" | bc`
-    tmp_free=`/usr/bin/df -m "$tmppath" | sed -e /Filesystem/d | awk '{print $3}'`
+    typeset -i total_req=`echo "(512)" | bc`
+    tmp_free=`/usr/bin/df -m "$tmppath" | /usr/bin/sed -e /Filesystem/d | /usr/bin/awk '{print $3}'`
     if [[ $tmp_free -le $total_req ]]
     then
       chfs -a size=+$(( total_req - tmp_free ))M "$tmppath"
       if [[ $? -ne 0 ]];
       then
-        echo "Please make sure $tmppath has around 340MB of free space to download and"
+        echo "Please make sure $tmppath has around 512MB of free space to download and"
         echo "extract files from dnf_bundle."
         exit 1
       fi
@@ -161,7 +148,7 @@ fi
 # Check if /opt is having enough space to install the packages from dnf_bundle.
 # Currently we need around 457MB of free space in /opt filesystem.
 typeset -i total_opt=`echo "(512)" | bc`
-opt_free=`/usr/bin/df -m /opt | sed -e /Filesystem/d | head -1 | awk '{print $3}'`
+opt_free=`/usr/bin/df -m /opt | /usr/bin/sed -e /Filesystem/d | /usr/bin/head -1 | /usr/bin/awk '{print $3}'`
 if [[ $opt_free -le $total_opt ]]
 then
     chfs -a size=+$(( total_opt - opt_free ))M /opt
@@ -262,16 +249,11 @@ fi
 if [[ $aix_730_plus -eq 1 ]]
 then
     echo "\nExtracting dnf_bundle_aix_73.tar ..."
-    tar -xvf dnf_bundle_aix_73.tar
+    /usr/bin/tar -xvf dnf_bundle_aix_73.tar
 else
     echo "\nExtracting dnf_bundle_aix_71_72.tar ..."
-    tar -xvf dnf_bundle_aix_71_72.tar
+    /usr/bin/tar -xvf dnf_bundle_aix_71_72.tar
 fi
-
-echo "Argument passing to the installation is:"
-echo $arg
-echo $yum4
-echo $yum3_instd
 
 ./install_dnf.sh "$arg" $yum4 $yum3_instd 2
 if [[ $? -eq 0 ]]
