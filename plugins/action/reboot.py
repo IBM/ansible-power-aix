3cafd91dfb7209bc4dd304dd61ad523bc92f26a0
schamola <shreyansh.chamola@ibm.com>
2024-02-13 12:42:09 +0530

    Modified reboot action and playbook
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- plugins/action/reboot.py
+++ plugins/action/reboot.py
@@ -69,51 +69,51 @@ class ActionModule(ActionBase):
 
         return reboot_result
 
-    def validate_reboot(self, test_command, reboot_timeout=None, action_kwargs=None):
-        self._display.vvv('Validating reboot....')
-        result = {}
+    # def validate_reboot(self, test_command, reboot_timeout=None, action_kwargs=None):
+    #     self._display.vvv('Validating reboot....')
+    #     result = {}
 
-        try:
-            if reboot_timeout is None:
-                reboot_timeout = 300
+    #     try:
+    #         if reboot_timeout is None:
+    #             reboot_timeout = 300
 
-            self.do_until_success_or_timeout(
-                action_desc="post-reboot test command",
-                reboot_timeout=reboot_timeout,
-                test_command=test_command,
-                action_kwargs=action_kwargs)
+    #         self.do_until_success_or_timeout(
+    #             action_desc="post-reboot test command",
+    #             reboot_timeout=reboot_timeout,
+    #             test_command=test_command,
+    #             action_kwargs=action_kwargs)
 
-            result['rebooted'] = True
-            result['changed'] = True
-            result['msg'] = "System has been rebooted SUCCESSFULLY"
+    #         result['rebooted'] = True
+    #         result['changed'] = True
+    #         result['msg'] = "System has been rebooted SUCCESSFULLY"
 
-        except TimedOutException:
-            result['failed'] = True
-            result['rebooted'] = True
-            result['msg'] = "Reboot Validation failed due to timeout"
-            return result
+    #     except TimedOutException:
+    #         result['failed'] = True
+    #         result['rebooted'] = True
+    #         result['msg'] = "Reboot Validation failed due to timeout"
+    #         return result
 
-        return result
+    #     return result
 
-    def do_until_success_or_timeout(self, action_desc, reboot_timeout, test_command, action_kwargs=None):
-        max_end_time = datetime.utcnow() + timedelta(seconds=reboot_timeout)
-        if action_kwargs is None:
-            action_kwargs = {}
-        if test_command is None:
-            test_command = 'whoami'
+    # def do_until_success_or_timeout(self, action_desc, reboot_timeout, test_command, action_kwargs=None):
+    #     max_end_time = datetime.utcnow() + timedelta(seconds=reboot_timeout)
+    #     if action_kwargs is None:
+    #         action_kwargs = {}
+    #     if test_command is None:
+    #         test_command = 'whoami'
 
-        while datetime.utcnow() < max_end_time:
-            try:
-                result = self._low_level_execute_command(test_command, sudoable=True)
-                if result['rc'] == 0:
-                    return
-            except Exception as e:
-                if isinstance(e, AnsibleConnectionFailure):
-                    try:
-                        self._connection.reset()
-                    except AttributeError:
-                        pass
-        raise TimedOutException("Connection reset failed while validating the reboot.")
+    #     while datetime.utcnow() < max_end_time:
+    #         try:
+    #             result = self._low_level_execute_command(test_command, sudoable=True)
+    #             if result['rc'] == 0:
+    #                 return
+    #         except Exception as e:
+    #             if isinstance(e, AnsibleConnectionFailure):
+    #                 try:
+    #                     self._connection.reset()
+    #                 except AttributeError:
+    #                     pass
+    #     raise TimedOutException("Connection reset failed while validating the reboot.")
 
     def run(self, tmp=None, task_vars=None):
         self._supports_async = True
@@ -146,13 +146,13 @@ class ActionModule(ActionBase):
             result['elapsed'] = str(elapsed.seconds) + ' sec'
             return result
 
-        if self.post_reboot_delay != 0:
-            self._display.vvv("waiting for post reboot delay of {delay} seconds".format(delay=self.post_reboot_delay))
-            time.sleep(self.post_reboot_delay)
+        # if self.post_reboot_delay != 0:
+        #     self._display.vvv("waiting for post reboot delay of {delay} seconds".format(delay=self.post_reboot_delay))
+        #     time.sleep(self.post_reboot_delay)
 
-        result = self.validate_reboot(test_command, reboot_timeout, action_kwargs=None)
+        # result = self.validate_reboot(test_command, reboot_timeout, action_kwargs=None)
 
-        elapsed = datetime.utcnow() - start
-        result['elapsed'] = str(elapsed.seconds) + ' sec'
+        # elapsed = datetime.utcnow() - start
+        # result['elapsed'] = str(elapsed.seconds) + ' sec'
 
         return result
