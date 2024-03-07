e79fac45c2c485e6fea49fc0f5534686137ce8b4
Shreyansh Chamola <shreyansh.chamola@ibm.com>
2024-03-07 12:20:01 +0530

    Added lsrsrc IBM.MCP info (#421)
    * Added lsrsrc IBM.MCP info
    
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>
    
    * Modified a bit
    
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>
    
    * Fixed linting issues
    
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>
    
    * Added documentation for IBM.MCP_info
    
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>
    
    ---------
    
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- plugins/modules/lpar_facts.py
+++ plugins/modules/lpar_facts.py
@@ -273,12 +273,38 @@ ansible_facts:
           type: dict
           elements: dict
           contains:
-           oslevel:
+            oslevel:
               description:
               - OperatingSystemBaseVersion TechnologyLevel ServicePack Build (vrmf).
               returned: always
               type: dict
               sample: '"oslevel": { "build": 2147, "sp": 3, "tl": 2, "base": "7.2.0.0" }'
+        IBM.MCP_info:
+          description: lsrsrc IBM.MCP command's output
+          returned: always
+          type: dict
+          elements: dict
+          contains:
+            resource list:
+              description: Resource Persistent Attributes for IBM.MCP
+              returned: always
+              type: dict
+              sample:
+                "resource 1": {
+                    "ActiveCommMode": "1",
+                    "ActivePeerDomain": "",
+                    "ConnectivityNames": "{127.0.0.1}",
+                    "HMCAddIPs": "",
+                    "HMCAddIPv6s": "",
+                    "HMCIPAddr": "127.0.0.1",
+                    "HMCName": "Va87f6d*XXXXXXX",
+                    "IPAddresses": "{127.0.0.1}",
+                    "KeyToken": "somehmc",
+                    "MNName": "127.0.0.1",
+                    "NodeID": "12622478031XXXXXX",
+                    "NodeNameList": "{somedomain.com}",
+                    "RMCKey": "64b686a1XXXXXXXX"
+                }
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -337,8 +363,50 @@ descr2key = {
     "NX Crypto Acceleration": ('nxcrypto_acc', 'bool'),
     "In-Core Crypto Acceleration": ('inc_core_crypto', 'bool'),
     "Full Core": ('full_coredump', 'bool'),
-    "oslevel": ('oslevel', 'str')
+    "oslevel": ('oslevel', 'str'),
+    "IBM.MCP_info": ('IBM.MCP_info', 'str')
 }
+
+
+def parse_MCP_info(stdout):
+    """
+    Utility function to parse the information retrieved from "lsrsrc IBM.MCP" command
+    arguments:
+      stdout - Standard output of the command
+    returns:
+      parsed_info (dict) - Parsed stdout in the form of a dictionary
+    """
+    parsed_info = {}
+    lines = stdout.splitlines()
+    current_resource = ""
+    resource_info = {}
+
+    for line in lines:
+        if ":" not in line and "=" not in line:
+            continue
+        if ":" in line:
+            if current_resource:
+                parsed_info[current_resource] = resource_info
+                resource_info = {}
+                current_resource = line.strip()[:-1]
+            else:
+                current_resource = line.strip()[:-1]
+        elif "=" in line:
+            id, val = line.split("=")
+            if id[:2] == "\t":
+                id = id[2:]
+            id = id.strip()
+            val = val.strip()
+            if id in ["NodeID", "ActiveCommMode"]:
+                val = int(val)
+            else:
+                val = ''.join(val.split('\"'))
+            resource_info[id] = val
+
+    if current_resource not in parsed_info.keys():
+        parsed_info[current_resource] = resource_info
+
+    return parsed_info
 
 
 def main():
@@ -350,6 +418,8 @@ def main():
     lparstat_path = module.get_bin_path('lparstat', required=True)
     prtconf_path = module.get_bin_path('prtconf', required=True)
     oslevel_path = module.get_bin_path('oslevel', required=True)
+    lsrsrc_path = module.get_bin_path('lsrsrc', required=True)
+
     cmd = [lparstat_path, '-is']
     ret, stdout, stderr = module.run_command(cmd, check_rc=True)
     ''' prtconf to get the following:
@@ -373,8 +443,14 @@ def main():
     ret1, stdout1, stderr1 = module.run_command(cmd, check_rc=True)
     stdout1 = "oslevel: " + stdout1
 
+    cmd = [lsrsrc_path, 'IBM.MCP']
+    ret2, stdout2, stderr2 = module.run_command(cmd, check_rc=True)
+
     stdout = stdout + "\n" + stdout1
+
     lparstat = {}
+    lparstat["IBM.MCP_info"] = parse_MCP_info(stdout2)
+
     for line in stdout.splitlines():
         if ':' not in line:
             continue
