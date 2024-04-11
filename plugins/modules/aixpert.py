367c8f54bc54cdba8dd0ad120391427e36f7d21e
Nitish K Mishra <nitismis@in.ibm.com>
2024-04-11 19:47:18 +0530

    lints
    Signed-off-by: Nitish K Mishra <nitismis@in.ibm.com>

--- plugins/modules/aixpert.py
+++ plugins/modules/aixpert.py
@@ -5,9 +5,9 @@
 # Copyright: (c) 2020- IBM, Inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import absolute_import, division, print_function
 import os
 import re
-from __future__ import absolute_import, division, print_function
 from ansible.module_utils.basic import AnsibleModule
 
 __metaclass__ = type
