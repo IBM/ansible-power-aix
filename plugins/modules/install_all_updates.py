a06ac788e0733699f84bad81cbfb714afd1501b2
schamola <shreyansh.chamola@ibm.com>
2025-02-22 14:15:49 +0530

    Added type and default in check_mode
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- plugins/modules/install_all_updates.py
+++ plugins/modules/install_all_updates.py
@@ -80,6 +80,8 @@ options:
   check_mode:
     description:
     - Performs a preview of an action by running all preinstallation checks for the specified action. No software changes are made.
+    type: bool
+    default: False
 notes:
   - You can refer to the IBM documentation for additional information on the installp command at
     U(https://www.ibm.com/support/knowledgecenter/ssw_aix_73/i_commands/install_all_updates.html)
