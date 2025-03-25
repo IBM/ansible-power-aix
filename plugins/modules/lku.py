559ef59b298fcdb1be59411c36a4fd0fd2f621ab
Nitish K Mishra <nitismis@in.ibm.com>
2025-03-25 14:27:44 +0530

    Include project name in LKU module
    Signed-off-by: Nitish K Mishra <nitismis@in.ibm.com>

--- plugins/modules/lku.py
+++ plugins/modules/lku.py
@@ -39,11 +39,16 @@ options:
     - Username for the PowerVC
     type: str
     required: true
+  PVC_project:
+    description:
+    - Specifies the project for the LKU operation, if not specified will take default project
+    type: str
+    default: None
   directory:
     description:
     - Path of the directory where fixes and filesets are present.
     type: str
-    default: null
+    default: None
   filesets_fixes:
     description:
     - Space separated names of filesets and interim fixes to be installed from provided I(directory).
@@ -51,7 +56,7 @@ options:
     - If you want to install all the updates and interim fixes then give input as all.
     - If you want to install only updates and not interim fixes then give input as update_all.
     type: str
-    default: null
+    default: None
 '''
 
 EXAMPLES = r'''
@@ -107,8 +112,12 @@ def authenticate_PVC(module):
     pvc_name = module.params['PVC_name']
     pvc_passwd = module.params['PVC_password']
     pvc_user = module.params['PVC_user']
+    pvc_project = module.params['PVC_project']
 
-    cmd = f'pvcauth -a {pvc_name} -u {pvc_user} -p {pvc_passwd}'
+    if pvc_project is None:
+        cmd = f'pvcauth -a {pvc_name} -u {pvc_user} -p {pvc_passwd}'
+    else:
+        cmd = f'pvcauth -a {pvc_name} -o {pvc_project} -u {pvc_user} -p {pvc_passwd}'
 
     rc, stdout, stderr = module.run_command(cmd)
 
@@ -159,6 +168,7 @@ def main():
             PVC_name=dict(type='str', required=True),
             PVC_password=dict(type='str', required=True, no_log=True),
             PVC_user=dict(type='str', required=True),
+            PVC_project=dict(type='str', required=False, default=None),
             directory=dict(type='str', required=False, default=None),
             filesets_fixes=dict(type='str', required=False, default=None),
         ),
