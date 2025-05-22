fcfab8321a156ba3fb18397a24ff92d7e74ae738
Panos <ptsiamis@gmail.com>
2025-05-22 17:20:32 +0300

    Update flrtvc.py to fix emgr not found issue
    This small workarround resolves a race condition that reports emgr.txt not found

--- plugins/modules/flrtvc.py
+++ plugins/modules/flrtvc.py
@@ -986,10 +986,15 @@ def run_flrtvc(flrtvc_path, params, force):
     emgr_file = os.path.join(workdir, 'emgr.txt')
     if os.path.exists(emgr_file):
         os.remove(emgr_file)
-    run_emgr(emgr_file)
-
+    
     # Wait until threads finish
+    # Silly fix of emgr.txt not found race condition issue
     wait_all()
+    time.sleep(1)
+    run_emgr(emgr_file)
+    wait_all()
+    time.sleep(1)
+    # Silly fix of emgr.txt not found issue end
 
     if not os.path.exists(lslpp_file) or not os.path.exists(emgr_file):
         if not os.path.exists(lslpp_file):
