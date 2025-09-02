cbfe03adce4d980b8b10c78ad15a8e7beb85ae23
schamola <shreyansh.chamola@ibm.com>
2025-09-02 15:19:52 +0530

    Fixed sanity test failures
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- plugins/modules/emgr.py
+++ plugins/modules/emgr.py
@@ -775,7 +775,7 @@ def main():
             results['changed'] = True
         elif action == 'list' and not module.params['preview'] and not module.check_mode and (rc == 0):
             results['ifix_details'] = parse_ifix_details(stdout)
-        
+
         # Adding system oslevel to output
         rc, stdout, stderr = module.run_command('usr/bin/oslevel -s')
 
@@ -783,7 +783,7 @@ def main():
             results['msg'] = "Failed to retrieve the oslevel from the system. Check stderr for more details."
             results['stderr'] = stderr
             module.fail_json(**results)
-        
+
         results['oslevel'] = stdout.rstrip()
 
     else:
