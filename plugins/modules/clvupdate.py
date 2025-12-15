bd0b7ac6dc4d6e37bdf41a2eb1f0a10e73121673
Nitish K Mishra <nitismis@in.ibm.com>
2025-12-15 14:12:08 +0530

    added GPL license
    Signed-off-by: Nitish K Mishra <nitismis@in.ibm.com>

--- plugins/modules/clvupdate.py
+++ plugins/modules/clvupdate.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright: (c) 2020- IBM, Inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
