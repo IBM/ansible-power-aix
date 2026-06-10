6acf391f5381dfde687e99127421942e89261878
schamola <shreyansh.chamola@ibm.com>
2026-06-10 20:31:03 +0530

    Fixed issues in nim
    Signed-off-by: schamola <shreyansh.chamola@ibm.com>

--- plugins/modules/nim.py
+++ plugins/modules/nim.py
@@ -203,7 +203,7 @@ status:
     returned: if the operation requires a target list
     type: dict
     contains:
-        <target>:
+        target:
             description: Status of the execution on the <target>.
             returned: when target is actually a NIM client
             type: str
@@ -236,7 +236,7 @@ meta:
             type: list
             elements: str
             sample: see below
-        <target>:
+        target:
             description: Detailed information on the execution on the C(target).
             returned: when target is actually a NIM client
             type: dict
@@ -267,7 +267,7 @@ meta:
             returned: only for show action
             type: dict
             contains:
-                <nim_object>:
+                nim_object:
                     description: Information for each individual NIM object fetched.
                     returned: if NIM object of this type exists
                     type: dict
